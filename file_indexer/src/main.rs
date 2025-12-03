2f965cd705e7efb293bb9a6f53570559d4942ef7
Serhii Shchudlo <serhii.shchudlo@kaongroup.com>
2025-12-03 11:14:42 +0200

    fixup! [TASK5] File indexer: added multithreading

--- file_indexer/src/main.rs
+++ file_indexer/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 use std::io;
 use std::io::BufRead;
 use std::path::{Path, PathBuf};
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::{mpsc, Mutex};
 use std::thread;
 use serde_json;
 use anyhow::Result;
@@ -18,38 +18,26 @@ fn main() -> Result<()> {
         .map(|n| n.get())
         .unwrap_or(3);
     let (tx_results, rx_results) = mpsc::channel::<(String, HashMap<String, Vec<usize>>)>();
-    let rx = Arc::new(Mutex::new(files));
+    let rx = Mutex::new(files);
 
     thread::scope(|scope| {
-        let threads: Vec<_> = (0..max_threads_count)
-            .map(|_| {
-                let rx = Arc::clone(&rx);
-                let tx = tx_results.clone();
-                scope.spawn(move || loop {
-                    let next_path = {
-                        rx.lock().unwrap().pop()
-                    };
+        for _ in 0..max_threads_count {
+            scope.spawn(|| loop {
+                let next_path = &rx.lock().unwrap().pop();
 
-                    match next_path {
-                        Some(path) => {
-                            if let Ok(map) = index_file(&path) {
-                                let _ = tx.send((path.display().to_string(), map));
-                            }
+                match next_path {
+                    Some(path) => {
+                        if let Ok(map) = index_file(&path) {
+                            let _ = tx_results.send((path.display().to_string(), map));
                         }
-                        None => break,
                     }
-                })
-            })
-            .collect();
-
-        drop(tx_results);
-
-        for thread in threads {
-            if let Err(e) = thread.join() {
-                println!("Worker thread error: {e:?}");
-            }
+                    None => break,
+                }
+            });
         }
     });
+
+    drop(tx_results);
 
     let mut result_map: HashMap<String, HashMap<String, Vec<usize>>> = HashMap::new();
     for (file, file_index) in rx_results {
